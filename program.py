0a82494879918e75fbf31dccc70bd845ae5c7f5b
Johannes Hofmeister <github@account.cessor.de>
2014-12-12 16:47:24 +0100

    Moved template generation to use jinja2 rather than my own poor generator

--- program.py
+++ program.py
@@ -1,18 +1,65 @@
-import sys
-from antwortlexer import *
-from antwortparser import *
-from visitor import *
+"""
+Antwort
 
-class AntwortTestParser(AntwortParser):
-    def __init__(self, input):
-        lexer = AntwortLexer(input)
-        LOOKAHEAD = 2
-        super(AntwortTestParser, self).__init__(lexer, LOOKAHEAD)
+Usage: 
+    my_program --template=<template> [--in=INFILE] [--out=<outfile>]
+
+Options:
+    -i INFILE, --in=INFILE              The input file written in the ANTWORT language
+    -t TEMPLATE, --template=TEMPLATE    The template file that defines the transformations
+    -o OUTFILE, --out=OUTFILE           Path of the file you would like to create
+"""
+
+import sys
+import codecs
+from jinja2 import Template
+from docopt import docopt
+
+from antwortlexer import AntwortLexer 
+from antwortparser import AntwortParser
+
+def parse(input):
+    lexer = AntwortLexer(input)
+    LOOKAHEAD = 2
+    return AntwortParser(lexer, LOOKAHEAD).parse()
+
+#open = open
+def read(path):
+    with open(path, 'r') as file:
+        return file.read()
+
+def read_utf(path):
+    with codecs.open(path,'r','utf-8') as file:
+        return file.read()
+
+def write_utf(path, content):
+    with codecs.open(path,'w', 'utf-8-sig') as file: 
+        file.write(content)
+
+def remove_empty_lines(text):
+    lines = text.split('\n')
+    lines = [line for line in lines if line.strip()]
+    return '\n'.join(lines)
+
+def transform(template, data):
+    template = Template(template)
+    template = template.render(questions=data.questions)
+    template = remove_empty_lines(template)
+    return template
 
 if __name__ == '__main__':
-    stdin = sys.stdin.read()
-    parser = AntwortTestParser(stdin)
-    tree = parser.parse()
-    visitor = HtmlVisitor()
-    tree.walk(visitor)
-    print visitor.buff.decode('utf-8').encode('cp1252')
+    arguments = docopt(__doc__)
+
+    infile = arguments['--in']
+    content = (read(infile) if infile else sys.stdin.read())
+    data = parse(content)
+
+    template = arguments['--template']
+    template = read_utf(template)
+    template = transform(template, data)
+
+    outfile = arguments['--out']
+    if outfile:
+        write_utf(outfile, template)
+    else:
+        sys.stdout.write(template)
